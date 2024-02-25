cd2c5109ae0bc2894a6221a54b2b03af2d887bab
David Palacios <davidpal3c@gmail.com>
2024-02-24 17:26:22 -0700

    contact app boilerplate done

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,9 +1,11 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-const ContactForm = () => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState("")
+const ContactForm = ({ existingContact = {}, updateCallback }) => {
+    const [firstName, setFirstName] = useState(existingContact.firstName || "");
+    const [lastName, setLastName] = useState(existingContact.lastName || "");
+    const [email, setEmail] = useState(existingContact.email || "");
+
+    const updating = Object.entries(existingContact).length !== 0
 
     const onSubmit = async (e) => {
         e.preventDefault()
@@ -11,31 +13,30 @@ const ContactForm = () => {
         const data = {
             firstName,
             lastName,
-            email,
+            email
         }
-        const url = 'http://127.0.0.1:5000/create_contact'
+        const url = "http://127.0.0.1:5000/" + (updating ? `update_contact/${existingContact.id}` : "create_contact")
         const options = {
-            method: "POST",
+            method: updating ? "PATCH" : "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
         }
         const response = await fetch(url, options)
-        if (response.status !== 201 && response.status !== 201){
+        if (response.status !== 201 && response.status !== 200) {
             const data = await response.json()
             alert(data.message)
         } else {
-            //successful response
+            updateCallback()
         }
-
     }
 
     return (
         <form onSubmit={onSubmit}>
             <div>
                 <label htmlFor="firstName">First Name:</label>
-                <input 
+                <input
                     type="text"
                     id="firstName"
                     value={firstName}
@@ -44,24 +45,25 @@ const ContactForm = () => {
             </div>
             <div>
                 <label htmlFor="lastName">Last Name:</label>
-                <input 
+                <input
                     type="text"
-                    id="LastName"
+                    id="lastName"
                     value={lastName}
                     onChange={(e) => setLastName(e.target.value)}
                 />
             </div>
             <div>
                 <label htmlFor="email">Email:</label>
-                <input 
+                <input
                     type="text"
                     id="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
             </div>
-            <button type="submit">Create Contact</button>
-        </form>)
-}
+            <button type="submit">{updating ? "Update" : "Create"}</button>
+        </form>
+    );
+};
 
-export default ContactForm 
+export default ContactForm
